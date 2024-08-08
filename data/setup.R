7986ad7f87ad29012d53995aff4a172631bfae7a
Nic Crane <thisisnic@gmail.com>
2024-08-08 15:30:51 +0100

    Update data/setup.R

--- data/setup.R
+++ data/setup.R
@@ -1,6 +1,6 @@
 # Set up links to the data directory
 
 system(sprintf("ln -s /mnt/shared/data/nyc-taxi %s/data/nyc-taxi", getwd()))
-system(sprintf("ln -s /mnt/shared/data/seattle-library-checkout.csv %s/data/seattle-library-checkout.csv", getwd()))
+system(sprintf("ln -s /mnt/shared/data/seattle-library-checkout.csv %s/data/seattle-library-checkouts.csv", getwd()))
 system(sprintf("ln -s /mnt/shared/data/taxi_zone_lookup.csv %s/data/taxi_zone_lookup.csv", getwd()))
 system(sprintf("ln -s /mnt/shared/data/taxi_zones %s/data/taxi_zones", getwd()))
